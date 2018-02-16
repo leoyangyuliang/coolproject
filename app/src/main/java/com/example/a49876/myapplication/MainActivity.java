ec5adf57c36120aec13aed8fa61460000638f8b8
yangyuliang <yangyuliang@csu.fullerton.edu>
2018-02-15 16:38:02 -0800

    Revert "Revert "commit for pull origin""
    This reverts commit cbf6f1fe7807c1d22d57055612786c7bd1cb0e5f.

--- app/src/main/java/com/example/a49876/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a49876/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView theDate;
     private Button btnGoCalendar;
     private Button btnGoAllJournal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
                startActivity(intent);
            }
         });
-
+        btnGoAllJournal.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, allJournalActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
