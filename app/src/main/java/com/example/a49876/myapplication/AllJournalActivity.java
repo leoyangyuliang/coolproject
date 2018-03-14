bb3e990b40dfb590972dae3b5dbe2267fa44f6a7
yangyuliang <yangyuliang@csu.fullerton.edu>
2018-03-13 18:12:09 -0700

    added read and write binary file

--- app/src/main/java/com/example/a49876/myapplication/AllJournalActivity.java
+++ app/src/main/java/com/example/a49876/myapplication/AllJournalActivity.java
@@ -3,12 +3,24 @@ package com.example.a49876.myapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.File;
 
 public class AllJournalActivity extends AppCompatActivity {
+    private String[] myary= {"a","b","c"};
     private static final String TAG = "CalendarActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        String[] ary;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_journal);
+        File file = getFilesDir();
+        File path = new File(file, "alljournal.bin");
+        WriteObject writeobject = new WriteObject();
+        writeobject.writeToBinary(myary, getFilesDir(), "alljournal.bin");
+        ReadObject readobject = new ReadObject();
+        ary =  (String[]) readobject.readFromBinary(path);
+        Log.e("testing", ary[0]);
     }
 }
--- app/src/main/java/com/example/a49876/myapplication/WriteObject.java
+++ app/src/main/java/com/example/a49876/myapplication/WriteObject.java
@@ -1,8 +1,37 @@
 package com.example.a49876.myapplication;
 
-/**
- * Created by 49876 on 3/13/2018.
- */
 
-public class WriteObject {
+
+        import java.io.FileOutputStream;
+        import java.io.ObjectOutputStream;
+        import java.io.*;
+
+public class WriteObject{
+
+    private File path;
+    private FileOutputStream outFile;
+    private ObjectOutputStream outStream;
+
+
+    //CONSTUCTOR
+    public WriteObject() {
+    }
+
+    //METHOD
+    public void writeToBinary(Object obj, File path, String fileName) {
+        // combind path+filename
+        this.path = new File(path,fileName);
+
+        //writing binary file
+        try { // try to open and write the file
+            outFile = new FileOutputStream(this.path);
+            outStream = new ObjectOutputStream(outFile);
+            outStream.writeObject(obj);
+            outStream.close();
+        } // try to open and write the file
+        catch (IOException ex) { // catch
+            System.out.println(ex); // display error msg in a TextArea, or use S.O.P. to console
+        } // catch
+    }
+
 }
