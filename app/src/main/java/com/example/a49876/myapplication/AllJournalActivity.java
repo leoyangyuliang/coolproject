9f4251c147417a61ae13ed73461f96ca6fa008ea
yangyuliang <yangyuliang@csu.fullerton.edu>
2018-03-18 21:44:54 -0700

    All journal list view
    pretty much finish but need work on click event

--- app/src/main/java/com/example/a49876/myapplication/AllJournalActivity.java
+++ app/src/main/java/com/example/a49876/myapplication/AllJournalActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class AllJournalActivity extends AppCompatActivity {
     private ListView allJournalListView;
-    private String[] myary= {"a","b","c"};
+    private ArrayList<String> alljournalslist;
+    private String[] alljournals;
 
     private static final String TAG = "AllJournalActivity";
     @Override
@@ -20,19 +24,34 @@ public class AllJournalActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_journal_layout);
+        FileUtils fileutils = new FileUtils();
 
+        File path = getFilesDir();
+        File file = new File(path, "AllJournals.bin");
+
+
+        //get all journals arraylist
+        alljournalslist = (ArrayList)fileutils.readFromBinary(file);
+
+        //convert arraylist to array
+        if(!alljournalslist.isEmpty()) {
+            Log.e("alljournal(0)",alljournalslist.get(0));
+
+            alljournals = alljournalslist.toArray(new String[alljournalslist.size()]);
+        }
+        //create list view
         allJournalListView = (ListView) findViewById(R.id.allJournalListView);
-
-
-
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.textview, myary);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.textview, alljournals);
         allJournalListView.setAdapter(adapter);
-//        File file = getFilesDir();
-//        File path = new File(file, "alljournal.bin");
-//        WriteObject writeobject = new WriteObject();
-//        writeobject.writeToBinary(myary, getFilesDir(), "alljournal.bin");
-//        ReadObject readobject = new ReadObject();
-//        ary =  (String[]) readobject.readFromBinary(path);
-//        Log.e("testing", ary[0]);
+        allJournalListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
     }
+
+
+
 }
