aee56b204fc8463862bcdbfc043f209a23423a8a
yangyuliang <yangyuliang@csu.fullerton.edu>
2018-03-18 18:50:17 -0700

    created list view for all journal
    Listview created successfully

--- app/src/main/java/com/example/a49876/myapplication/AllJournalActivity.java
+++ app/src/main/java/com/example/a49876/myapplication/AllJournalActivity.java
@@ -4,24 +4,35 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.File;
 
 public class AllJournalActivity extends AppCompatActivity {
+    private ListView allJournalListView;
     private String[] myary= {"a","b","c"};
-    private static final String TAG = "CalendarActivity";
+
+    private static final String TAG = "AllJournalActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        String[] ary;
+        setTitle("All Journals");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_journal_layout);
 
-        File file = getFilesDir();
-        File path = new File(file, "alljournal.bin");
-        WriteObject writeobject = new WriteObject();
-        writeobject.writeToBinary(myary, getFilesDir(), "alljournal.bin");
-        ReadObject readobject = new ReadObject();
-        ary =  (String[]) readobject.readFromBinary(path);
-        Log.e("testing", ary[0]);
+        allJournalListView = (ListView) findViewById(R.id.allJournalListView);
+
+
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.textview, myary);
+        allJournalListView.setAdapter(adapter);
+//        File file = getFilesDir();
+//        File path = new File(file, "alljournal.bin");
+//        WriteObject writeobject = new WriteObject();
+//        writeobject.writeToBinary(myary, getFilesDir(), "alljournal.bin");
+//        ReadObject readobject = new ReadObject();
+//        ary =  (String[]) readobject.readFromBinary(path);
+//        Log.e("testing", ary[0]);
     }
 }
