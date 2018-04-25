990c6a7d85a285bdfd8dc024aee7535df06575d3
yangyuliang <yangyuliang@csu.fullerton.edu>
2018-04-24 23:04:11 -0700

    connected to firebase
    able to connect to firebase for authntication.

---
+++ app/src/main/java/com/example/a49876/myapplication/LogInActivity.java
@@ -0,0 +1,108 @@
+package com.example.a49876.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by 49876 on 4/24/2018.
+ */
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener{
+    private FirebaseAuth mAuth;
+    private TextView email, password;
+    private Button login, signup;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(this);
+        signup = findViewById(R.id.signup);
+        signup.setOnClickListener(this);
+    }
+
+
+    //signin
+    public void logIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e("Login", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.e("login", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+
+
+
+
+
+
+    private void updateUI(FirebaseUser user) {
+
+        if (user != null) {
+                Log.e("login","success");
+        } else {
+            Log.e("login","fail");
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button) v;
+        switch(b.getId()) {
+            case R.id.login:
+            {
+                logIn(email.getText().toString(),password.getText().toString());
+                break;
+            }
+            case R.id.signup:
+            {
+                Log.e("signup","clicked");
+                break;
+            }
+        }
+
+
+    }
+
+}
+
