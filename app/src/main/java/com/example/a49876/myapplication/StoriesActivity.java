004390e58c1f99071fc39bee75d4b81325dd1d05
yangyuliang <yangyuliang@csu.fullerton.edu>
2018-05-09 18:53:54 -0700

    add/delete public journals
    users can delete the journal from public by unclick the "share to public" opton, and click save.
    
    and modify public journals but the likes will go to zero
    
    when people go to journal page, if its already shared, the click box will be marked

--- app/src/main/java/com/example/a49876/myapplication/StoriesActivity.java
+++ app/src/main/java/com/example/a49876/myapplication/StoriesActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -14,9 +13,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,20 +21,20 @@ import java.util.List;
  * Created by 49876 on 5/2/2018.
  */
 
-public class PublicJournalsActivity extends AppCompatActivity{
+public class StoriesActivity extends AppCompatActivity{
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private FirebaseFirestore db;
     //List<String[]> and List<List<String>> both doesn't work, weird thing happen in line
-    //57 - 66,  in the for-loop everything is normal, but outside of for-loop doesn't work.
+    //57 - 66,  in the for-loop everything is normal, but outside of for-loop the list loses values.
     //I guess it is one of the world unsolve sevret(joking)
     private List<ArrayList<String>> mDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Stories");
+        setTitle("Community");
         setContentView(R.layout.recycler_view_layout);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mDataset = new ArrayList<ArrayList<String>>();
@@ -60,13 +57,15 @@ public class PublicJournalsActivity extends AppCompatActivity{
                     if (document.exists()) {
                         for(String key: document.getData().keySet())
                         {
+                            if(document.getData().get(key)=="")
+                            continue;
                             ArrayList<String> l = new ArrayList<>();
                             l.clear();
                             l = (ArrayList<String>)document.getData().get(key);
                             mDataset.add(l);
                         }
                         // specify an adapter (see also next example)
-                        mAdapter = new MyAdapter(mDataset,PublicJournalsActivity.this);
+                        mAdapter = new StoriesAdapter(mDataset,StoriesActivity.this);
                         mRecyclerView.setAdapter(mAdapter);
 
                     } else {
